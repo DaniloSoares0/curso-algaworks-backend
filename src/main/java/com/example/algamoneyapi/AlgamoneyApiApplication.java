7672a18f7f9006633c514669916d3cc378461462
DaniloSoares0 <daniloxviii@gmail.com>
2021-11-18 20:40:43 -0300

    perparando deploy

--- src/main/java/com/example/algamoneyapi/AlgamoneyApiApplication.java
+++ src/main/java/com/example/algamoneyapi/AlgamoneyApiApplication.java
@@ -2,13 +2,18 @@ package com.example.algamoneyapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.PropertySource;
+
+import com.example.algamoneyapi.config.property.AlgamoneyApiProperty;
 
 @SpringBootApplication
-public class AlgamoneyApiApplication {
+@PropertySource("application-prod.properties")
+@EnableConfigurationProperties(AlgamoneyApiProperty.class)
+public class AlgamoneyApiApplication  {
 
 	public static void main(String[] args) {
-		
-		SpringApplication.run(AlgamoneyApiApplication.class, args);
+		SpringApplication.run(AlgamoneyApiApplication.class, args);	
 	}
 
 }
--- src/main/java/com/example/algamoneyapi/config/property/AlgamoneyApiProperty.java
+++ src/main/java/com/example/algamoneyapi/config/property/AlgamoneyApiProperty.java
@@ -3,14 +3,22 @@ package com.example.algamoneyapi.config.property;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@Data
 @ConfigurationProperties("algamoney")
 public class AlgamoneyApiProperty {
+
+	private String originPermitida = "http://localhost:8000";
 
 	private final Seguranca seguranca = new Seguranca();
 	
 	@Data
-	public static class Seguranca{
+	public static class Seguranca {
+
 		private boolean enableHttps;
+
 	}
+	
+	
 }
