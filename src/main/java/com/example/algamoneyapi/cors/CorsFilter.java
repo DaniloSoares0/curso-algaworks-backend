5598870e9fcb33673957792ee858db9802e7857b
DaniloSoares0 <daniloxviii@gmail.com>
2021-11-24 20:30:31 -0300

    Mudando para prod

--- src/main/java/com/example/algamoneyapi/cors/CorsFilter.java
+++ src/main/java/com/example/algamoneyapi/cors/CorsFilter.java
@@ -18,7 +18,7 @@ import org.springframework.stereotype.Component;
 @Order(Ordered.HIGHEST_PRECEDENCE)
 public class CorsFilter implements Filter {
 
-	private String originPermitida =  "http://localhost:8000"; //TODO Configurar para diferentes ambientes 
+	private String originPermitida =  "https://estudos-spring.herokuapp.com"; //TODO Configurar para diferentes ambientes 
 	
 	@Override
 	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
